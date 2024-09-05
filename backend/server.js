4be3c45c1d7eded94a4a7332f6fa303c137aff15
Harsh Yadav <harrshyadav24@gmail.com>
2024-09-06 01:59:06 +0530

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,12 @@ if (process.env.NODE_ENV == 'production') {
   const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
+  
+  console.log("before app get *");
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+);
+console.log("after app get *");
 } 
 else {
   const __dirname = path.resolve();
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -10,7 +10,7 @@
 
 
 
-export const BASE_URL     = 'https://electro-mart-gamma.vercel.app/'; 
+export const BASE_URL     = 'http://electro-mart-gamma.vercel.app/'; 
 export const PRODUCTS_URL = 'api/products';       
 export const USERS_URL    = 'api/users';
 export const ORDERS_URL   = 'api/orders';
